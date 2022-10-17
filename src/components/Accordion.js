544e814a8c0185b9b2b9a2a99cbef5c10d569c2c
Stas <leshuksu@gmail.com>
2022-10-17 23:54:31 +0300

    Added Semantic UI styles for accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,17 +3,18 @@ import React from "react";
 const Accordion = (props) => {
     const renderedItems = props.items.map(({ title, content }) => {
         return (
-            <div>
-                <div>
+            <React.Fragment key={title}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
                     {title}
                 </div>
-                <div>
+                <div className="content active">
                     <p>{content}</p>
                 </div>
-            </div>
+            </React.Fragment>
         )
     })
-    return <h1>{renderedItems}</h1>;
+    return <div className="ui styled accordion">{renderedItems}</div>;
 }
 
 export default Accordion
