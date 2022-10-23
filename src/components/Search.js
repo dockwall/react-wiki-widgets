161fa8d395ab76a3c027700339431fe8dcf1073f
Stas <leshuksu@gmail.com>
2022-10-23 15:42:05 +0300

    Made request to wiki after every search term change, used axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
     const [searchValue, setSearchValue] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
-        console.log('Rerendered')
+        const sendRequest = async () => {
+            const { data } = await axios.get('https://ru.wikipedia.org/w/api.php',
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: searchValue,
+                    },
+                })
+
+            setSearchResults(data.query.search);
+        }
+
+        if (searchValue) sendRequest();
     }, [searchValue])
 
     return (
