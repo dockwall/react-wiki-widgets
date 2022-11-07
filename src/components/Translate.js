451521237a31ed761e9920c9481cb2ab071a08ab
Stas <leshuksu@gmail.com>
2022-11-07 23:39:08 +0300

    Added controlled text input for text to translate

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,13 +18,24 @@ const translateOptions = [
 
 const Translate = () => {
     const [selectedLanguage, setSelectedLanguage] = useState(translateOptions[0]);
+    const [translateText, setTranslateText] = useState('')
 
     return (
         <div>
-            <h1>Hello!</h1>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter a term to translate</label>
+                    <input
+                        className="input"
+                        value={translateText}
+                        onChange={(e) => setTranslateText(e.target.value)}
+                    >
+                    </input>
+                </div>
+            </div>
             <Dropdown
                 options={translateOptions}
-                labelText="Select a language to translate"
+                labelText="Select a language"
                 selected={selectedLanguage}
                 onSelectedChange={setSelectedLanguage}
             />
