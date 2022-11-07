0e6fdbd5b7d014c596956e97c568a2c45e0410cd
Stas <leshuksu@gmail.com>
2022-11-08 00:27:20 +0300

    Added restriction - translate works only on localhost:3000

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -20,6 +20,16 @@ const Translate = () => {
     const [selectedLanguage, setSelectedLanguage] = useState(translateOptions[0]);
     const [translateText, setTranslateText] = useState('')
 
+    // Google API key has a one restriction - it works correctly only if host is local and port is 3000
+    if (window.location.host !== 'localhost:3000') {
+        return (
+            <div>
+                <h1>In this host there is no possibility of this widget work</h1>
+                <h2>Please use localhost:3000 instead</h2>
+            </div>
+        );
+    }
+
     return (
         <div>
             <div className="ui form">
