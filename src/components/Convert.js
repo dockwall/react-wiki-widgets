4024e931eeb7e17141eb3ede18283e61bcee0ff9
Stas <leshuksu@gmail.com>
2022-11-08 15:27:58 +0300

    Add render of Google API response (translated text)

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// API key in axios works only at the localhost:3000
+
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState('')
+
+
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2',
-            {}, {
-            params: {
-                q: text,
-                target: language.value,
-                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+        if (!text) return;
+
+        const sendRequest = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2',
+                {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+                }
             }
+            )
+
+            setTranslated(data.data.translations[0].translatedText)
         }
-        )
+
+        sendRequest()
+
+
     }, [language, text])
     return (
         <div>
-            <h2>{language.value}</h2>
-            <h2>{text}</h2>
+            <h3 className="ui header">{translated}</h3>
         </div>
     );
 }
