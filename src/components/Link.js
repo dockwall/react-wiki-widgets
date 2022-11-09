42f05461f8562221baf6ab6da8ead90388de39a3
Stas <leshuksu@gmail.com>
2022-11-09 18:47:45 +0300

    Wired up links and Route. Navigation is done

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,9 @@ import React from "react";
 const Link = ({ path, children }) => {
     const onLinkClick = (e) => {
         e.preventDefault();
+        window.history.pushState({}, '', path);
+        const navEvent = new PopStateEvent('popstate')
+        window.dispatchEvent(navEvent);
     }
 
     return <a
